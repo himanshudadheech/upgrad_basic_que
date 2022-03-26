e5ba67a7dd332b5eabd7ba51cd341575658ef028
Himanshu Dadheech <19etccs026@technonjr.org>
2022-03-26 15:55:01 +0530

    update que2 solution

--- que2.js
+++ que2.js
@@ -1,7 +1,15 @@
-const a = [52, 42, 82, 93, 98];
-
-a.sort();
-console.log("min");
-console.log(a[0]);
-console.log("max");
-console.log(a[a.length - 1]);
+// const a = [95, -3, -1, 1, 2, 3, 4, 5, -4];
+const a = [-2, -1, 0, 1, 2];
+let min = a[0];
+let max = a[1];
+for (let i = 0; i < a.length; i++) {
+  if (min > a[i]) {
+    min = a[i];
+  } else {
+    if (max < a[i]) {
+      max = a[i];
+    }
+  }
+}
+console.log("min :" + min);
+console.log("max :" + max);
